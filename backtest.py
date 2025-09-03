33ae564f68df967882d97189c0940085a0c7544a
Saroop <ssjagdev@outlook.com>
2025-09-03 23:46:02 +0100

    spy cumulative returns graph

--- backtest.py
+++ backtest.py
@@ -2,8 +2,14 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 spy = yf.Ticker("SPY")
-spy_historical = spy.history(start="2025-09-02", end="2025-09-03", interval="15m")
-data = yf.download("SPY", start="2025-09-02", end="2025-09-03", interval="15m")
-print(data)
+data = yf.download("SPY", start="2020-01-01", end=datetime.today().strftime('%Y-%m-%d'), interval="1wk")
+cumulative_returns = ((data['Close'] / data['Close'].iloc[0])*100) - 100
+print(cumulative_returns)
+
+plt.figure(figsize=(10, 6))
+plt.plot(cumulative_returns.index, cumulative_returns, label='Cumulative Returns')
+plt.title('Cumulative Returns of SPY')
+plt.show()
